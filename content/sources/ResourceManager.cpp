d231c882b1c73e6398a6068d75c7d0a3639dc215
Antoine Busque <antoinebusque@gmail.com>
2011-12-23 17:22:29 -0500

    ResourceManager complete, tout compile. Reste a tester.

---
+++ content/sources/ResourceManager.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+
+#include "Asset.hpp"
+#include "ResourceManager.hpp"
+
+using namespace std;
+
+template <typename T>
+ResourceManager<T>::ResourceManager(string path) : _path(path)
+{
+}
+
+template <typename T>
+ResourceManager<T>::~ResourceManager()
+{
+}
+
+template <typename T>
+void ResourceManager<T>::add(string filename)
+{
+    _resources.push_back(Asset<T>(filename, _path));
+}
+
