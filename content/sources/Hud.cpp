113aad9925ade67c40979b74857e2a242db0d344
Antoine Busque <antoinebusque@gmail.com>
2012-01-03 02:57:11 -0500

    Added fps to hud

--- content/sources/Hud.cpp
+++ content/sources/Hud.cpp
@@ -9,8 +9,10 @@ const string Hud::FONT_NAME = "terminus.ttf";
 Hud::Hud(Game* game) : DrawableComponent(game)
 {
     _bus = _game->getBus();
+    _fpsCounter = _game->getFpsCounter();
     _score = new TextComponent(_game, "0", FONT_NAME, Vector2f(0,0));
     _speed = new TextComponent(_game, "0", FONT_NAME, Vector2f(0,0));
+    _fps = new TextComponent(_game, "0", FONT_NAME, Vector2f(0,0));
 }
 
 Hud::~Hud()
@@ -21,22 +23,33 @@ void Hud::update()
 {
     string score = convertIntToString(_bus->getScore());
     string speed = convertIntToString(_bus->getSpeed());
+    string fps = convertFloatToString(_fpsCounter->getFps());
 
     _score->setText(score);
     _speed->setText(speed);
+    _fps->setText(fps);
 
     _speed->setPosition(Vector2f(_app->GetWidth() - _speed->getWidth(), 0));
+    _fps->setPosition(Vector2f(_app->GetWidth() - _fps->getWidth(), _app->GetHeight() - _fps->getHeight()));
 }
 
 void Hud::draw()
 {
     _score->draw();
     _speed->draw();
+    _fps->draw();
 }
 
 string Hud::convertIntToString(int n)
 {
     stringstream ss;
     ss << n;
+    return ss.str();
+}
+
+string Hud::convertFloatToString(float n)
+{
+    stringstream ss;
+    ss << setiosflags(ios::fixed) << setprecision(0) << n;
     return ss.str();
 }
--- content/sources/Hud.hpp
+++ content/sources/Hud.hpp
@@ -3,8 +3,10 @@
 
 #include <SFML/Graphics.hpp>
 #include <sstream>
+#include <iomanip>
 #include "DrawableComponent.hpp"
 #include "Bus.hpp"
+#include "FpsCounter.hpp"
 #include "TextComponent.hpp"
 
 class Hud : public DrawableComponent
@@ -20,9 +22,12 @@ public :
 private :
 
     Bus* _bus;
+    FpsCounter* _fpsCounter;
     TextComponent* _score;
     TextComponent* _speed;
+    TextComponent* _fps;
     std::string convertIntToString(int n);
+    std::string convertFloatToString(float n);
 };
 
 #endif
