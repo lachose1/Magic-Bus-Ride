1525a65808a9223a0d07c0085e3dd3eda69cedda
Hugo Laliberte <hugolaliberte@gmail.com>
2012-01-06 01:29:44 -0500

    Calice de git

--- content/sources/Game.cpp
+++ content/sources/Game.cpp
@@ -134,7 +134,7 @@ void Game::run()
 
 		if(_inputManager->isNewKey(InputManager::P))
 		   _paused = !_paused;
-
+		
 		updateWorld();
 		_app->Clear();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
