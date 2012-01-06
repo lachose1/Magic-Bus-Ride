8e2ee550488c6a75feeb99b3020f9dad906708d2
Hugo Laliberte <hugolaliberte@gmail.com>
2012-01-05 23:30:51 -0500

    Rajout de quelques fonctions

--- content/sources/Menu.cpp
+++ content/sources/Menu.cpp
@@ -4,12 +4,27 @@
 using namespace std;
 using namespace sf;
 
-Menu::Menu(Game* game, string logo, float positionX, float positionY, vector<DrawableComponent*> elements) : Component(game)
+Menu::Menu(Game* game, string logo, float positionX, float positionY, vector<DrawableComponent*> elements) : Component(game), _elements(elements)
 {
 	_position.x = positionX;
 	_position.y = positionY;
+	_app = _game->getApp();
+	_logoImage = _game->getImageManager()->find(logo);
+	_logoSprite.SetImage(*(_logoImage));
+	_logoSprite.SetPosition(_position.x-_logoImage->GetWidth(), _position.y);
 }
 
 Menu::~Menu()
+{
+}
+
+void Menu::draw()
+{
+	_app->Draw(_logoSprite);
+	for(unsigned int i = 0; i < _elements.size(); i++)
+		_elements[i]->draw();
+}
+
+void Menu::update()
 {
 }
--- content/sources/Menu.hpp
+++ content/sources/Menu.hpp
@@ -1,6 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <SFML/Graphics.hpp>
 #include "Component.hpp"
 #include "DrawableComponent.hpp"
 
@@ -10,12 +11,18 @@ class Menu : public Component
 {
 public :
 
-    Menu(Game* game, std::string logo, float positionX, float positionY, std::vector<DrawableComponent*> elements);
+	Menu(Game* game, std::string logo, float positionX, float positionY, std::vector<DrawableComponent*> elements);
     ~Menu();
+	void draw();
+	void update();
     
 private :
 
 	sf::Vector2f _position;
+	std::vector<DrawableComponent*> _elements;
+	sf::RenderWindow* _app;
+	sf::Image* _logoImage;
+	sf::Sprite _logoSprite;
 };
 
 #endif
